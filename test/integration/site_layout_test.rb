b57912bfaa821a5a6ee7051e273ca91b61f901a9
Sean Chon <chonsean@gmail.com>
2015-03-28 13:14:20 -0700

    Complete up to 8.1.3

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get signup_path
     assert_select "title", full_title("Sign up")
+    get login_path
+    assert_select "a[href=?]", signup_path
   end
 end
